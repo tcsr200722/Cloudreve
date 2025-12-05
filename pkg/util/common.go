05c68b40625ca1af995c567eac3df677829ce27c
Darren Yu <YDL@YUDONGLING.NET>
2025-12-05 15:57:58 +0800

    fix(thumb blob path): separators be wrongly modified (#3062) (#3116)
    * fix(thumb blob path): separators be wrongly modified
    
    * Update common.go

--- pkg/util/common.go
+++ pkg/util/common.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"path"
 	"path/filepath"
 	"regexp"
 	"strconv"
@@ -101,7 +102,7 @@ func Replace(table map[string]string, s string) string {
 
 // ReplaceMagicVar 动态替换字符串中的魔法变量
 func ReplaceMagicVar(rawString string, fsSeparator string, pathAvailable bool, blobAvailable bool,
-					 timeConst time.Time, userId int, originName string, originPath string, completeBlobPath string) string {
+	timeConst time.Time, userId int, originName string, originPath string, completeBlobPath string) string {
 	re := regexp.MustCompile(`\{[^{}]+\}`)
 	return re.ReplaceAllStringFunc(rawString, func(match string) string {
 		switch match {
@@ -164,7 +165,7 @@ func ReplaceMagicVar(rawString string, fsSeparator string, pathAvailable bool, b
 			return match
 		case "{blob_path}":
 			if blobAvailable {
-				return filepath.Dir(completeBlobPath) + fsSeparator
+				return path.Dir(completeBlobPath) + fsSeparator
 			}
 			return match
 		default:
